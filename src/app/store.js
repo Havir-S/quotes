2156642e7bca00dfa0415e3ad148dfa14e0039e1
Havir <saffronkrzysztof@gmail.com>
2022-03-18 03:02:19 +0100

    Signed-off-by: Havir <saffronkrzysztof@gmail.com>

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { createStore } from '@reduxjs/toolkit';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const reducer = (x) => {
+  return {}
+}
+
+export const store = createStore(reducer);
